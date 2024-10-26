7e202fb9c2d9cdaf9f856a42cc7c78f2e733feda
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-10-26 10:28:10 +0300

    init

---
+++ fbot/src/main/java/brocodex/fbot/FbotApplication.java
@@ -0,0 +1,13 @@
+package brocodex.fbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbotApplication.class, args);
+	}
+
+}
