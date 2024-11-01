3716cbd6bbb2db55b03f85238c9518031e85a9d2
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-11-01 12:04:06 +0300

    prepared DTOs

---
+++ src/test/java/brocodex/fbot/repository/TransactionTest.java
@@ -0,0 +1,9 @@
+package brocodex.fbot.repository;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TransactionTest {
+}
---
+++ src/test/java/brocodex/fbot/service/ReportTest.java
@@ -0,0 +1,4 @@
+package brocodex.fbot.service;
+
+public class ReportTest {
+}
