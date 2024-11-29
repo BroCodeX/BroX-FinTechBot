6ad96c5df2b3d0606aaf2ea52fa9f3dad381d003
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-11-29 11:11:32 +0300

    corrected budget service and mapper

--- src/main/java/brocodex/fbot/service/BudgetService.java
+++ src/main/java/brocodex/fbot/service/BudgetService.java
@@ -2,6 +2,7 @@ package brocodex.fbot.service;
 
 import brocodex.fbot.dto.budget.BudgetDTO;
 import brocodex.fbot.dto.user.UserDTO;
+import brocodex.fbot.mapper.BudgetMapper;
 import brocodex.fbot.repository.BudgetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,17 @@ public class BudgetService {
     @Autowired
     private BudgetRepository repository;
 
+    @Autowired
+    private BudgetMapper mapper;
+
     public BudgetDTO showBudget(Long id) {
         return null;
     }
 
     public BudgetDTO createBudget(BudgetDTO dto) {
-        return null;
+        var budget = mapper.map(dto);
+        repository.save(budget);
+        return mapper.map(budget);
     }
 
     public BudgetDTO updateBudget(Long id, BudgetDTO dto) {
